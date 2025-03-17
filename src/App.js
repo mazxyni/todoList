9b40c3e70b6fd7863e5249fea600585df0c5fa16
mazxyni <ajinrhdwn@gmail.com>
2025-03-18 00:07:34 +0900

    TodoItem 컴포넌트 이벤트 추가까지 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
 import React from 'react';
+import TodoListTemplate from './components/js/TodoListTemplate';
+import Form from './components/js/Form';
+import TodoItemList from './components/js/TodoItemList';
  
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.id = 2;
+        this.state = {
+            input: "",
+            todos: [
+                { id: 0, content: '리액트를 공부하자0', isComplete: false },
+                { id: 1, content: '리액트를 공부하자1', isComplete: true }
+            ]
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+ 
+    handleChange(event) {
+        this.setState({
+            input: event.target.value
+        });
+    }
+ 
+    handleCreate() {
+        const { input, todos } = this.state;
+        if (input === "") {
+            alert("오늘 할 일을 입력해주세요!");
+            return;
+        }
+        this.setState({
+            input: "",
+            todos: todos.concat({
+                id: this.id++,
+                content: input,
+                isComplete: false
+            })
+        });
+    }
+ 
+    handleKeyPress(event) {
+        if (event.key === "Enter") {
+            this.handleCreate();
+        }
+    }
+ 
     render() {
         return (
-            <div>
-                To-do List 를 만들어보겠습니다!
-            </div>
+            <TodoListTemplate form={(
+                <Form
+                    value={this.state.input}
+                    onChange={this.handleChange}
+                    onCreate={this.handleCreate}
+                    onKeyPress={this.handleKeyPress} />
+            )}>
+                <TodoItemList todos={this.state.todos} />
+            </TodoListTemplate>
         );
     }
 }
